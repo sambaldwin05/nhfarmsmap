7baf11cf5e2003259402c2a14907f6e19b806b6a
Sam Baldwin <sam.baldwin.05@gmail.com>
2016-12-15 22:03:19 -0500

    First stab at map addition

---
+++ js/example.js
@@ -0,0 +1,31 @@
+// mapbox connection set up stuff.
+var mapId = 'sambaldwin05.019h34oc';
+var accessToken = 'pk.eyJ1Ijoic2FtYmFsZHdpbjA1IiwiYSI6ImNpbnE0dDJqZzEwNDJ0cWtqdWM5MXpsb3oifQ.V9cC7ebF_7CRrd5tkmIoBA';
+// Create the map object with your mapId and token,
+// referencing the DOM element where you want the map to go.
+L.mapbox.accessToken = accessToken;
+var map = L.mapbox.map('map', mapId);
+
+// Set the initial view of the map to the whole US
+map.setView([44, -71.6], 8);
+
+// Great, now we have a basic web map!
+var dataFileToAdd = 'data/nh_farms.geojson';
+
+// Buildinate our feature layer.
+var featureLayer = L.mapbox.featureLayer();
+    featureLayer.loadURL(dataFileToAdd);
+    featureLayer.addTo(map);
+
+
+featureLayer.on('ready', function(){
+  this.eachLayer(function(layer){
+    // console.log(layer);
+    layer.setIcon(L.mapbox.marker.icon({
+      "marker-color": "#8834bb",
+      "marker-size": "small",
+      "marker-symbol": "farm"
+    }));
+    // layer.bindPopup('Welcome to ' + layer.feature.properties.name);
+  });
+});  
